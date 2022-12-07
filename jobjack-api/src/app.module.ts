f88ed9aa3d66f25017e07c855351d90ab35a38e8
neld02 <deon.nel@vcontractor.co.za>
2022-12-07 21:01:59 +0200

    dependency fix

--- jobjack-api/src/app.module.ts
+++ jobjack-api/src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module, Logger } from '@nestjs/common';
 import { JobjackModule } from './jobjack/jobjack.module';
-import { PromModule } from '@digikare/nestjs-prom';
 
 @Module({
   imports: [
-    JobjackModule,
-    PromModule.forRoot({
-      defaultLabels: {
-        app: 'Jobjack Assessment',
-        version: '1.0.0',
-      },
-      withHttpMiddleware: {
-        enable: true,
-      }
-    })
+    JobjackModule
   ],
   controllers: [],
   providers: [Logger],
